8dfc7e69941b66198f9f158f4c02542005672e67
Danilo Lazic <dalazrap@gmail.com>
2024-04-01 19:50:31 +0200

    Random forest classifier, somethings not properly working...

--- randforestclass.py
+++ randforestclass.py
@@ -5,3 +5,24 @@ from sklearn.metrics import accuracy_score
 
 # Loadin and preprocessing the dataset
 data = pd.read_csv("archive/breed_traits.csv")
+
+# Separating features (X) and target variable (y)
+x = data.drop('Breed', axis=1)  # Features
+y = data['Breed']  # Target variable
+
+# Splitting the dataset into training and testing
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# Training the model
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_classifier.fit(x_train.values, y_train.values)
+
+# Model evaluation
+y_pred = rf_classifier.predict(x_test.values)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+# Making predictions
+new_data = [[5,5,4,3,2,2,1,1,4,4,4,4,5,5,3,5]]
+predicted_breed = rf_classifier.predict(new_data)
+print("Predicted Breed:", predicted_breed)
